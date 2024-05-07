43318d84d435c612dddd5001474e825b889d42c4
Abhay Verma <138868836+AbhayVerma19@users.noreply.github.com>
2024-05-07 18:50:41 +0530

    Add files via upload

---
+++ Tetris/position.h
@@ -0,0 +1,14 @@
+#ifndef POSITION_H_INCLUDED
+#define POSITION_H_INCLUDED
+
+#pragma once
+
+class Position
+{
+public:
+    Position(int row, int column);
+    int row;
+    int column;
+};
+
+#endif // POSITION_H_INCLUDED
